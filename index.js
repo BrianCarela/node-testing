7374bc4f5785d153c16920debb0a9e258f503523
Brian Carela <BrianCarela@gmail.com>
2018-04-13 15:07:38 -0400

    trying to use express to connect the audio filepath easily

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs'); // change ejs to html for html files
+app.set('audio', __dirname + '/audio');
 
 app.get('/', function(request, response) {
   response.render('pages/index');
