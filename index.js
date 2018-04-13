e7456aea109a460f2d9cbd285c229769ae0efed8
Brian Carela <BrianCarela@gmail.com>
2018-04-13 16:05:49 -0400

    tried a couple routing things, no juice

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ app.use(express.static(__dirname + '/public'));
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'html'); // changed ejs to html for html files
-app.set('audio', __dirname + '/audio');
+// app.set('audio', __dirname + '/audio'); // this wasn't working
+
+// const audio = require('./audio'); // testing something - this is treated as a module, and does not register the same way that a controller would. This method of testing failed
+// app.use('/audio', audio); // testing something
 
 app.get('/', function(request, response) {
   response.render('pages/index');
