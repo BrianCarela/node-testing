1da977c8b2a05095b70aea20b6f678956b0ba215
Brian Carela <BrianCarela@gmail.com>
2018-04-13 16:28:42 -0400

    FIRST STEP DONE

--- index.js
+++ index.js
@@ -7,11 +7,16 @@ app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
-app.set('view engine', 'html'); // changed ejs to html for html files
+app.set('view engine', 'ejs'); // changed ejs to html for html files, but it looks for the module, so that's fucked.
+
+
 // app.set('audio', __dirname + '/audio'); // this wasn't working
 
 // const audio = require('./audio'); // testing something - this is treated as a module, and does not register the same way that a controller would. This method of testing failed
 // app.use('/audio', audio); // testing something
+
+// declare audio directory to be used as a store for static files
+app.use('/audio', express.static(__dirname + '/audio'));
 
 app.get('/', function(request, response) {
   response.render('pages/index');
